c475bd8f97422fde0f8cb6521370d107a13c55f8
yuens <yuens.me@gmail.com>
2024-03-29 03:39:28 -0400

    save pt for a partial zoho booking custom langchain tool

--- src/prompt/chat_prompt_chain.py
+++ src/prompt/chat_prompt_chain.py
@@ -1,18 +1,24 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class ChatPrompt:
-    def __self__(self):
-        self.chat = ChatOpenAI(model="gpt-4-0125-preview", temperature=0.1)
+    def __init__(self):
+        self.chat = ChatOpenAI(
+            model="gpt-4-0125-preview", temperature=0.1, streaming=False
+        )
         self.prompt = ChatPromptTemplate.from_messages(
             [
                 (
                     "system",
-                    "You're a booking agent for a housing rental company. you're given a knowledge_base for property specific knowledge and a calendar tool for booking related functions. converse in user's language with an upbeat go-getter attitude while maintaining a professional composure",
+                    "You're a booking agent for a housing rental company. you're given a knowledge base for a specific property and a calendar tool for booking related functions. converse in user's language with an upbeat go-getter attitude while maintaining a professional composure",
                 ),
                 MessagesPlaceholder(variable_name="chat_history"),
                 ("human", "{input}"),
+                MessagesPlaceholder(variable_name="agent_scratchpad"),
             ]
         )
         self.chain = self.prompt | self.chat
